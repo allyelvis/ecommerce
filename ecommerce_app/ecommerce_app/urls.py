fed63e1b536d28842ee953846966d2ac8f651d33
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-07-26 20:12:50 +0200

    Update urls.py
    Update

--- ecommerce_app/ecommerce_app/urls.py
+++ ecommerce_app/ecommerce_app/urls.py
@@ -6,6 +6,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('invoices/', include('invoices.urls')),
-    path('', TemplateView.as_view(template_name="index.html"), name='home'),
+    path('ecommerce_app/invoices/templates/invoices/', include('invoices.urls')),
+    path('', TemplateView.as_view(template_name="ecommerce/index.html"), name='home'),
 ]
