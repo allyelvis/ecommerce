133b7227ccb8e758ee89aa132ed794d698bb4531
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-07-26 20:18:53 +0200

    Update admin.py
    Update

--- ecommerce_app/invoices/admin.py
+++ ecommerce_app/invoices/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+# invoices/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Invoice
+
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('invoice_number', 'customer_name', 'total_amount', 'date_created')
+    search_fields = ('invoice_number', 'customer_name')
+    list_filter = ('date_created',)
+    ordering = ('-date_created',)
+
+admin.site.register(Invoice, InvoiceAdmin)
